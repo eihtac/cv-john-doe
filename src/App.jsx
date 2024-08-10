e13ccec4a81e7a81e1f4b514d57f1069bee8d4f7
Cathie <flammant.cathie@gmail.com>
2024-08-10 15:12:50 +0200

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
 import "./index.css"
 
 const App = () => {
   return (
     <Router>
       <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
       <Footer/>
     </Router>
   )
